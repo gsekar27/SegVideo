c87f5009016658c06dc400b148d9ea40e8ed64d2
Ganmani Sekar <18008751+kannu27@users.noreply.github.com>
2017-08-30 00:57:50 -0700

    Update RenderVideo.java

--- RenderVideo.java
+++ RenderVideo.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.concurrent.TimeUnit;
-
 import javax.swing.*;
 
 
@@ -20,19 +19,12 @@ public class RenderVideo {
 		int width = Integer.parseInt(args[1]);
 		int height = Integer.parseInt(args[2]);
 		int rps = Integer.parseInt(args[3]);
-		//String outputFilename = "outputvideo.rgb";
-		
-		
 		img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		//int count =0;
 		try {
 			File file = new File(args[0]);
 			InputStream is = new FileInputStream(file);
-
 			long len = file.length();
 			byte[] bytes = new byte[(int)len];
-			//System.out.println(len + ","+bytes.length);
-
 			int offset = 0;
 			int numRead = 0;
 			while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
@@ -41,19 +33,8 @@ public class RenderVideo {
 			
 			is.close();
 			bytes = Segmentation.padHeight(bytes, 4);
-			//Calculate the pixel array and delay
-			//int numOfFrames = offset / (height * width *3);
-			//int[][][] pixArray = new int[numOfFrames][height][width];
-			
-			//int delay = 90;
-			 //Including some factor for the processing time	:0.85 for sd and 0.60 for hd
-			
+		
 			int delay = (int)((1000/rps));
-			
-					
-			//pixArray = createPixelsFromByteArray(bytes,height,width, numOfFrames);
-			
-			
 			//Render video
 			frame = new JFrame();
 			GridBagLayout gLayout = new GridBagLayout();
@@ -63,9 +44,7 @@ public class RenderVideo {
 			while(true){//To play the video in loop
 				long t = System.currentTimeMillis();
 				int ind = 0;
-				//int count= 0;
 				offset = bytes.length;
-				//System.out.println("starts: " + System.currentTimeMillis());
 				while(ind < offset ){
 					for(int y = 0; y < height; y++){					
 						for(int x = 0; x < width; x++){						
@@ -76,18 +55,13 @@ public class RenderVideo {
 
 							int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
 							img.setRGB(x,y,pix);
-							//int pix = ((a << 24) + (r << 16) + (g << 8) + b);
-							//pixArray[count][y][x] = pix;						
+							//int pix = ((a << 24) + (r << 16) + (g << 8) + b);				
 							ind++;
 						}	
 					}
-					//count++;
+
 					ind += 2 * height * width;
 					TimeUnit.MILLISECONDS.sleep(delay);	
-				
-//				
-				
-		
 				// Use labels to display the images			
 				JLabel lbText1 = new JLabel(result);
 				lbText1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -101,20 +75,15 @@ public class RenderVideo {
 				c.gridx = 0;
 				c.gridy = 0;
 				frame.getContentPane().add(lbText1, c);
-		
-				
 				c.fill = GridBagConstraints.HORIZONTAL;
 				c.gridx = 0;
 				c.gridy = 1;
 				frame.getContentPane().add(lbIm1, c);
-				
 				frame.pack();
 				frame.setVisible(true);	
 			
 			}
-				//long et = System.currentTimeMillis();
-				//System.out.println("Played for: " + (et - t)/1000 + " seconds");	
-		
+
 			}	
 		
 		} catch (FileNotFoundException e) {
@@ -122,10 +91,8 @@ public class RenderVideo {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch(ArrayIndexOutOfBoundsException e){
-			//System.out.println(count + "\n");
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	
